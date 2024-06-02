2530cbf9bc11cbaca623c0f2d088b3ae76b1ac6e
Antovlad <148706243+Antovlad@users.noreply.github.com>
2024-06-02 21:59:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,369 +1,302 @@
 #include <iostream>
-#include <cctype>
-#include <cstring>
+#include <vector>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <map>
+
 using namespace std;
-class medicament{
-	char nume[30];
-	int pret;
-	public:
-		medicament()
-			{
-				strcpy(nume, "Medicament");
-				pret=1;
-             }
-         int Pret() const
-         {
-             return pret;
-         }
-         void schimbarepret(int x)
-         {
-             pret=x;
-         }
-         void citeste()
-         	{
-         		cout<<"Denumire: ";
-         		cin.get();
-         		cin.getline(nume,30);
-         		cout<<"Pret: ";
-         		cin>>pret;
-         	}
-         friend ostream & operator << (ostream & out , const medicament & med)
-        {
-            out<<med.nume<<" : "<<med.pret<<" de lei."<<endl;
-            return out;
-        }
-        ~medicament(){
-         }
-        medicament(const medicament &a);
-        medicament & operator = (const medicament & med)
-        {
-        	pret=med.pret;
-        	strcpy(nume,med.nume);
-        	return *this;
-        }
 
+// Singleton Pattern pentru gestionarea unică a bibliotecii
+class Biblioteca;
+
+// Observer Pattern pentru actualizarea autorilor atunci când o carte nouă este adăugată
+class Observer {
+public:
+    virtual void update(const string &carte) = 0;
 };
-medicament::medicament(const medicament &a)
-{
-	strcpy(nume,a.nume);
-	pret=a.pret;
-}
-class pacient{
-	char nume[30];
-	int nrmed;
-	medicament a[10];
-	public:
-		pacient()
-		{
-			strcpy(nume,"Pacient");
-			nrmed=0;
-		}
-		int costuri() const
-		{
-			int s=0;
-			for (int i=0;i<nrmed;i++)
-			{
-				s=s+a[i].Pret();
-			}
-			return s;
-		}
-		void setNrmed(int x);
-		int getNrmed();
-		void citeste()
-		{
-			cin.getline(nume,30);
-		}
-		void setMed(medicament m, int i);
-		 friend ostream & operator << (ostream & out , const pacient & p)
-        {
-            for(int i=0;i<p.nrmed;i++)
-        		out<<p.a[i];
-        	out<<"Ele costa in total "<<p.costuri()<<" lei"<<endl;
-            return out;
-        }
-        void getnume(char s[30]) const
-        {
-        	strcpy(s,nume);
-        }
-        void citescnume()
-        {
-        	cin.getline(nume,30);
-        }
-        void meniu()
-        {
-        	cout<<"0. Intoarceti-va\n";
-        	cout<<"1. Prescrieti un medicament.\n";
-        	cout<<"2. Vizualizati lista de medicamente.\n";
-        	cout<<"3. Schimbati un medicament\n";
-        	cout<<"4. Copie medicament\n";
-        	int buton;
-        	cin>>buton;
-        	if(buton==0)
-        		;
-        	else
-                if(buton==1)
-                {
-                    (*this).prescriere();
-                    (*this).meniu();
-                }
-                else
-                    if(buton==2)
-                    {
-                        cout<<*this;
-                        (*this).meniu();
-                    }
-                    else
-                        if(buton==3)
-                        {
-                            medicament x;
-                            (*this).setMed(x,0);
-                            cout<<x;
-                            (*this).meniu();
-                        }
-                        else
-                        {
-                            medicament x(a[0]);
-                            cout<<x;
-                            (*this).meniu();
-                        }
-        }
-        void prescriere()
-        {
-        	nrmed++;
-        	a[nrmed-1].citeste();
-        }
+
+class Autor : public Observer {
+private:
+    string nume;
+    string nationalitate;
+    int anul_nasterii;
+public:
+    Autor(const string &n, const string &nat, int an) : nume(n), nationalitate(nat), anul_nasterii(an) {}
+
+    // Getters
+    string getNume() const { return nume; }
+    string getNationalitate() const { return nationalitate; }
+    int getAnulNasterii() const { return anul_nasterii; }
+
+    // Setters
+    void setNume(const string &n) { nume = n; }
+    void setNationalitate(const string &nat) { nationalitate = nat; }
+    void setAnulNasterii(int an) { anul_nasterii = an; }
+
+    // Metoda pentru afișarea informațiilor despre autor
+    void afiseazaInformatii() const {
+        cout << "Nume: " << nume << ", Nationalitate: " << nationalitate << ", Anul nasterii: " << anul_nasterii << endl;
+    }
+
+    void update(const string &carte) override {
+        cout << "Autorul " << nume << " a fost notificat despre adăugarea cărții: " << carte << endl;
+    }
 };
-void pacient::setNrmed(int x)
-{
-	nrmed=x;
-}
-void pacient::setMed(medicament m, int i)
-{
-	a[i]=m;
-}
-int pacient::getNrmed()
-{
-	return nrmed;
-}
-class medic{
-	char nume[30];
-	int nrpacienti;
-	pacient pacienti[20];
-	int program[5][8];
-	int salariu;
-	char specializare[30];
-	public:
-		medic()
-		{
-			strcpy(nume,"Medic");
-			nrpacienti=0;
-			for(int i=0;i<5;i++)
-				for(int j=0;j<8;j++)
-					program[i][j]=0;
-			strcpy(specializare,"Medic de familie");
-		}
-		friend ostream & operator << (ostream &out, const medic &medik)
-		{
-			char s[30];
-			for(int i=0;i<medik.nrpacienti;i++)
-			{
-				medik.pacienti[i].getnume(s);
-				out<<i+1<<". "<<s<<endl;
-			}
-			return out;
-		}
-		bool ocupat(int x,int y)
-		{
-			if (program[x-1][y-9]==1)
-				return true;
-			return false;
-		}
-		void getnume(char s[30]) const
-		{
-			strcpy(s,nume);
-		}
-		void citire()
-		{
-			cin.get();
-			cout<<"Nume: ";
-			cin.getline(nume,30);
-			cout<<"Specializare: ";
-			cin.getline(specializare,30);
-		}
-		void meniu()
-		{
-			int buton;
-			cout<<"Nume: "<<nume<<endl;
-			cout<<"Specializare: "<<specializare<<endl;
-			cout<<"1. Faceti programare."<<endl;
-			cout<<"2. Vizualizati lista pacientilor."<<endl;
-			cout<<"3. Intoarceti-va\n";
-			cin>>buton;
-			if(buton==1)
-			{
-				(*this).programare();
-				(*this).meniu();
-			}
-			else if(buton==2)
-			{
-				(*this).vizualizare();
-				(*this).meniu();
-			}
-			else ;
-		}
-		bool liber()
-		{
-			bool x=false;
-			for(int i=0;i<5;i++)
-				for(int j=0;j<8;j++)
-					if(program[i][j]==0)
-					{
-						x=true;
-						break;
-					}
-			return x;
-		}
-		void vizualizare()
-		{
-			int buton;
-			char s[30];
-			cout<<"0. Intoarceti-va.\n";
-			cout<<*this;
-			cin>>buton;
-			if(buton==0)
-				;
-			else
-            {
-				pacienti[buton-1].meniu();
-				cout<<"Ne intoarcem"<<endl;
-				(*this).vizualizare();
+
+class Carte {
+private:
+    string titlu;
+    string autor;
+    int anul_publicarii;
+    string gen;
+public:
+    Carte(const string &t, const string &a, int an, const string &g) : titlu(t), autor(a), anul_publicarii(an), gen(g) {}
+
+    // Getters
+    string getTitlu() const { return titlu; }
+    string getAutor() const { return autor; }
+    int getAnulPublicarii() const { return anul_publicarii; }
+    string getGen() const { return gen; }
+
+    // Setters
+    void setTitlu(const string &t) { titlu = t; }
+    void setAutor(const string &a) { autor = a; }
+    void setAnulPublicarii(int an) { anul_publicarii = an; }
+    void setGen(const string &g) { gen = g; }
+
+    // Metoda pentru afișarea informațiilor despre carte
+    void afiseazaInformatii() const {
+        cout << "Titlu: " << titlu << ", Autor: " << autor << ", Anul publicarii: " << anul_publicarii << ", Gen: " << gen << endl;
+    }
+};
+
+// Clasa iterator pentru iterat prin carti
+class Iterator {
+private:
+    vector<shared_ptr<Carte>>::iterator current;
+    vector<shared_ptr<Carte>>::iterator end;
+public:
+    Iterator(vector<shared_ptr<Carte>>::iterator start, vector<shared_ptr<Carte>>::iterator end) : current(start), end(end) {}
+
+    bool hasNext() {
+        return current != end;
+    }
+
+    shared_ptr<Carte> next() {
+        return *current++;
+    }
+};
+
+class Biblioteca {
+private:
+    vector<shared_ptr<Carte>> carti;
+    vector<shared_ptr<Autor>> autori;
+    static shared_ptr<Biblioteca> instanta;
+
+    Biblioteca() {}
+
+public:
+    static shared_ptr<Biblioteca> getInstanta() {
+        if (instanta == nullptr) {
+            instanta = shared_ptr<Biblioteca>(new Biblioteca());
+        }
+        return instanta;
+    }
+
+    void adaugaCarte(const shared_ptr<Carte> &carte) {
+        carti.push_back(carte);
+        for (auto &autor : autori) {
+            if (autor->getNume() == carte->getAutor()) {
+                autor->update(carte->getTitlu());
             }
-		}
-		void programare()
-		{
-			if(!(*this).liber())
-			{
-				cout<<"Programul medicului este plin pe saptamana asta. Incercati saptamana viitoare.";
-			}
-			else
-			{
-				cout<<"Alegeti ziua:"<<endl;
-				cout<<"1.Luni\n2.Marti\n3.Miercuri\n4.Joi\n5.Vineri\n";
-				int buton1,buton2;
-				cin>>buton1;
-				cout<<"Alegeti o ora de la 9 la 16.\n";
-				cin>>buton2;
-				if(program[buton1-1][buton2-9]==0)
-				{
-					cout<<"Introduceti numele pacientului.\n";
-					cin.get();
-					nrpacienti++;
-					pacienti[nrpacienti-1].citescnume();
-					program[buton1-1][buton2-9]=1;
-					cout<<"Ne intoarcem la meniul doctorului.\n";
-				}
-				else
-				{
-					cout<<"Domnul doctor este ocupat in acea zi si la acea ora. Incercati alta data.\n";
-				}
-			}
-		}
+        }
+    }
 
-};
-class spital
-{
-	char nume[30];
-	medic medici[5];
-	int nrmedici;
-	public:
-		spital()
-		{
-			nrmedici=0;
-			strcpy(nume,"Spital");
-		}
-		friend ostream & operator << (ostream &out, const spital &hospital)
-		{
-			char s[30];
-			out<<"0. Reveniti la ecranul principal."<<endl;
-            for(int i=0;i<hospital.nrmedici;i++)
-            {
-                hospital.medici[i].getnume(s);
-                out<<i+1<<"."<<s<<endl;
-            }
-			return out;
-		}
-		void vizualizare()
-		{
-			if(nrmedici==0)
-			{
-				cout<<"Spitalul inca nu a angajat medici. Alegeti alta varianta.\n";
-			}
-			else
-			{
-				char s[30];
-				int buton;
-				cout<<"0. Reveniti la ecranul principal."<<endl;
-				for(int i=0;i<nrmedici;i++)
-				{
-					medici[i].getnume(s);
-					cout<<i+1<<"."<<s<<endl;
-				}
-				cin>>buton;
-				if(buton==0)
-					;
-				else
-                {
-					medici[buton-1].meniu();
-					(*this).vizualizare();
-                }
-			}
-		  }
-		void angajare()
-		{
-			nrmedici++;
-			medici[nrmedici-1].citire();
-			cout<<"Ne intoarcem la meniul principal\n";
-		}
-		void setNrmed(int x)
-		{
-		    nrmedici=x;
-		}
-		void gestiune()
-		{
-			int buton;
-			cout<<"1. Vizualizare"<<endl;
-			cout<<"2. Angajare"<<endl;
-			cout<<"3. Inchidere"<<endl;
-			cout<<"4. Schimbare numar medici"<<endl;
-			cin>>buton;
-			if(buton==1)
-			{
-				(*this).vizualizare();
-				(*this).gestiune();
-			}
-			else
-                if(buton==2)
-                {
-                    (*this).angajare();
-                    (*this).gestiune();
-                }
-				else
-                    if(buton==4)
-                    {
-                        int x;
-                        cin>>x;
-                        setNrmed(x);
-                        cout<<"Sunt "<<x<<" medici."<<endl;
-                        (*this).gestiune();
-                    }
-                    else
-                        ;
+    void eliminaCarte(const string &titlu) {
+        carti.erase(remove_if(std::begin(carti), std::end(carti),
+                               [&titlu](const shared_ptr<Carte> &carte) { return carte->getTitlu() == titlu; }),
+                    std::end(carti));
+    }
 
-		}
+    shared_ptr<Carte> gasesteCarte(const string &titlu) {
+        auto it = find_if(std::begin(carti), std::end(carti),
+                          [&titlu](const shared_ptr<Carte> &carte) { return carte->getTitlu() == titlu; });
+        if (it != std::end(carti)) {
+            return *it;
+        }
+        return nullptr;
+    }
+
+    void afiseazaCarti() const {
+        for (const auto &carte : carti) {
+            carte->afiseazaInformatii();
+        }
+    }
+
+    void adaugaAutor(const shared_ptr<Autor> &autor) {
+        autori.push_back(autor);
+    }
+
+    Iterator getIterator() {
+        return Iterator(carti.begin(), carti.end());
+    }
+
+    vector<shared_ptr<Autor>> getAutori() const {
+        return autori;
+    }
 };
-int main()
-{
-	spital x;
-	x.gestiune();
-	return 0;
+
+shared_ptr<Biblioteca> Biblioteca::instanta = nullptr;
+
+// Funcție șablon pentru afișarea elementelor dintr-un container
+template<typename T>
+void afiseazaElemente(const T &container) {
+    for (const auto &element : container) {
+        element->afiseazaInformatii();
+    }
+}
+
+// Clasă șablon pentru un catalog de cărți
+template<typename T>
+class Catalog {
+private:
+    map<string, T> catalog;
+public:
+    void adaugaInCatalog(const string &cheie, const T &valoare) {
+        catalog[cheie] = valoare;
+    }
+
+    T gasesteInCatalog(const string &cheie) {
+        return catalog[cheie];
+    }
+
+    void afiseazaCatalogul() const {
+        for (const auto &element : catalog) {
+            element.second->afiseazaInformatii();
+        }
+    }
+};
+
+void afiseazaMeniu() {
+    cout << "\nMeniu Biblioteca:\n";
+    cout << "1. Adauga o carte\n";
+    cout << "2. Elimina o carte\n";
+    cout << "3. Gaseste o carte\n";
+    cout << "4. Afiseaza toate cartile\n";
+    cout << "5. Adauga un autor\n";
+    cout << "6. Afiseaza toti autorii\n";
+    cout << "0. Iesire\n";
+    cout << "Alege o optiune: ";
+}
+
+void adaugaCarte(shared_ptr<Biblioteca> &biblioteca) {
+    string titlu, autor
+    , gen;
+    int anul_publicarii;
+
+    cout << "Titlul cartii: ";
+    getline(cin, titlu);
+
+    cout << "Autorul cartii: ";
+    getline(cin, autor);
+
+    cout << "Anul publicarii: ";
+    cin >> anul_publicarii;
+
+    cout << "Genul cartii: ";
+    cin.ignore();
+    getline(cin, gen);
+
+    auto carte = make_shared<Carte>(titlu, autor, anul_publicarii, gen);
+    biblioteca->adaugaCarte(carte);
+    cout << "Cartea a fost adaugata cu succes.\n";
+}
+
+void eliminaCarte(shared_ptr<Biblioteca> &biblioteca) {
+    string titlu;
+    cout << "Introdu titlul cartii de eliminat: ";
+    cin.ignore();
+    getline(cin, titlu);
+
+    biblioteca->eliminaCarte(titlu);
+    cout << "Cartea a fost eliminata cu succes.\n";
+}
+
+void gasesteCarte(shared_ptr<Biblioteca> &biblioteca) {
+    string titlu;
+    cout << "Introdu titlul cartii de gasit: ";
+    cin.ignore();
+    getline(cin, titlu);
+
+    auto carte = biblioteca->gasesteCarte(titlu);
+    if (carte) {
+        cout << "Cartea a fost gasita:\n";
+        carte->afiseazaInformatii();
+    } else {
+        cout << "Cartea nu a fost gasita.\n";
+    }
+}
+
+void adaugaAutor(shared_ptr<Biblioteca> &biblioteca) {
+    string nume, nationalitate;
+    int anul_nasterii;
+
+    cout << "Numele autorului: ";
+    getline(cin, nume);
+    cin.ignore();
+    cout << "Nationalitatea autorului: ";
+    getline(cin, nationalitate);
+    cin.ignore();
+    cout << "Anul nasterii: ";
+    cin >> anul_nasterii;
+
+    auto autor = make_shared<Autor>(nume, nationalitate, anul_nasterii);
+    biblioteca->adaugaAutor(autor);
+    cout << "Autorul a fost adaugat cu succes.\n";
+}
+
+void afiseazaAutori(shared_ptr<Biblioteca> &biblioteca) {
+    cout << "Autori in biblioteca:\n";
+    for (const auto &autor : biblioteca->getAutori()) {
+        autor->afiseazaInformatii();
+    }
+}
+
+int main() {
+    auto biblioteca = Biblioteca::getInstanta();
+    int optiune;
+
+    do {
+        afiseazaMeniu();
+        cin >> optiune;
+
+        switch (optiune) {
+            case 1:
+                adaugaCarte(biblioteca);
+                break;
+            case 2:
+                eliminaCarte(biblioteca);
+                break;
+            case 3:
+                gasesteCarte(biblioteca);
+                break;
+            case 4:
+                biblioteca->afiseazaCarti();
+                break;
+            case 5:
+                adaugaAutor(biblioteca);
+                break;
+            case 6:
+                afiseazaAutori(biblioteca);
+                break;
+            case 0:
+                cout << "Iesire.\n";
+                break;
+            default:
+                cout << "Optiune invalida. Te rog sa incerci din nou.\n";
+        }
+    } while (optiune != 0);
+
+    return 0;
 }
